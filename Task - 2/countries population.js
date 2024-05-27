3455aa1b78797ca247727f1baff577481a51578e
praveenrames <praveenrameshr2@gmail.com>
2024-05-28 01:42:44 +0530

    first commit

---
+++ Task - 2/countries population.js
@@ -0,0 +1,17 @@
+const request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all")
+request.send()
+request.onload=function(){ 
+    if (request.status==200)
+{
+    let c=JSON.parse(request.responseText)
+    c.filter((s)=>(s.population<200000)).map((e)=>{
+    console.log(e.population, e.name.official); 
+})
+}
+    else   
+{ 
+    request.onerror=function()
+{
+    console.log("site is error")
+}}}
